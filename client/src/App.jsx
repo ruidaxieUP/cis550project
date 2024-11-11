4e593f3a9498426e9302809e1acc38f78d5bd0ec
Ley Li <rurui0916@gmail.com>
2024-11-11 18:28:23 -0500

    Implement routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import PersonPage from './pages/PersonPage';
+import MoviePage from './pages/MoviePage';
+import ContactPage from './pages/ContactPage';
 
 
 function App() {
 
   return (
     <>
-      <HomePage />
+      <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/person" element={<PersonPage />} />
+        <Route path="/movie" element={<MoviePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
     </>
   )
 }
