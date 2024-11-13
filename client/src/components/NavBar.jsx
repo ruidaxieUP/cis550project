f73b3e8f9c1d2810e8c105d901a41aa5856ceadb
Ley Li <rurui0916@gmail.com>
2024-11-13 17:35:42 -0500

    Add PersonInfoPage and MovieInfoPage components; update routing path

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,7 +10,13 @@ const NAV_ITEMS = [
   { name: "Contact", path: "/contact" },
 ];
 
-// Helper component for a navigation item
+NavItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  selectedPage: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 function NavItem({ name, path, selectedPage, onClick }) {
   const baseLinkStyle = 'flex pt-[8px] pr-[16px] pb-[8px] pl-[16px] gap-[8px] justify-center items-center flex-nowrap rounded-[8px] relative pointer';
   const activeStyle = `${baseLinkStyle} bg-[#f5f5f5] border-none`;
@@ -29,14 +35,6 @@ function NavItem({ name, path, selectedPage, onClick }) {
   );
 }
 
-// Define prop types for NavItem
-NavItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  path: PropTypes.string.isRequired,
-  selectedPage: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
 export default function NavBar() {
   const [selectedPage, setSelectedPage] = useState("Home");
 
@@ -46,16 +44,6 @@ export default function NavBar() {
 
   return (
     <div className='flex w-[1436px] pt-[16px] pr-[32px] pb-[16px] pl-[32px] gap-[24px] items-center flex-wrap bg-[#fff] border-solid border-t border-b border-[#d9d9d9] relative overflow-hidden mx-auto my-0' style={{ height: '108px' }}>
-      
-      {/* Logo/Icon */}
-      <div className='flex w-[44px] gap-[24px] items-center flex-nowrap relative z-[1]'>
-        <div className='flex w-[44px] pt-[12px] pr-[12px] pb-[12px] pl-[12px] gap-[8px] justify-center items-center shrink-0 flex-nowrap bg-[#f5f5f5] rounded-[32px] border-solid border border-[#d9d9d9] relative overflow-hidden'>
-          <div className='w-[20px] h-[20px] shrink-0 relative overflow-hidden'>
-            {/* Icon placeholder */}
-            <div className='w-[16.875px] h-[11.875px] bg-[length:100%_100%] bg-no-repeat relative mt-[4.063px] ml-[1.563px]' />
-          </div>
-        </div>
-      </div>
 
       {/* Navigation Links */}
       <div className='flex gap-[8px] justify-end items-start grow basis-0 flex-wrap relative'>
