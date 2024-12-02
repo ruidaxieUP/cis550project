9da0eefa61fd77fcf23014219081f649d7e02910
ruidaxieUP <81105889+ruidaxieUP@users.noreply.github.com>
2024-12-01 22:11:18 -0500

    Add movie info to MovieInfoPage (#17)
    * Add movie info
    
    * Correct typos
    
    ---------
    
    Co-authored-by: Ruida Xie <ruida.xie@sig.com>

--- client/src/pages/utils.jsx
+++ client/src/pages/utils.jsx
@@ -1,9 +1,19 @@
 export const fetchData = async (endpoint, setData) => {
     try {
-      const response = await fetch(`http://localhost:8080/api/${endpoint}`);
-      const data = await response.json();
-      setData(data);
+        const response = await fetch(`http://localhost:8080/api/${endpoint}`);
+        const data = await response.json();
+        setData(data);
     } catch (error) {
-      console.error(`Error fetching ${endpoint}:`, error);
+        console.error(`Error fetching ${endpoint}:`, error);
     }
-  };
+};
+
+export const fetchDataById = async (endpoint, id, setData) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/${endpoint}/${id}`);
+        const data = await response.json();
+        setData(data);
+    } catch (error) {
+        console.error(`Error fetching ${endpoint}:`, error);
+    }
+};
